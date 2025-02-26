ff53bbff48ffb8b4b4e1ed609120e22fd439e0db
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2025-02-26 21:29:55 +0700

    [REFACTOR] Move LocationPermissionPopup styling into a new file

--- app/components/LocationPermissionPopup.tsx
+++ app/components/LocationPermissionPopup.tsx
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React from "react";
+import { 
+  Overlay, 
+  Modal, 
+  WarningHeader, 
+  Message, 
+  ButtonContainer, 
+  Button 
+} from "../../styles/LocationPermissionPopup.styles";
 
 interface LocationPermissionPopupProps {
   open: boolean;
@@ -8,78 +15,8 @@ interface LocationPermissionPopupProps {
   onDeny: () => void;
 }
 
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 1000;
-`;
-
-const Modal = styled.div`
-  background: white;
-  border-radius: 12px;
-  text-align: center;
-  width: 350px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-`;
-
-const WarningHeader = styled.div`
-  background:#407BFF;
-  padding: 20px;
-  border-top-left-radius: 12px;
-  border-top-right-radius: 12px;
-  color: white;
-  font-weight: bold;
-  font-size: 1.5rem;
-`;
-
-const Message = styled.p`
-  margin: 20px 0;
-  font-size: 1rem;
-  color: #333;
-  padding: 0 10px;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-`;
-
-const Button = styled.button<{ $primary?: boolean }>`
-  padding: 9px 10px;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  font-size: 1rem;
-  font-weight: bold;
-  flex: 1;
-  margin: 0 5px;
-
-  color: ${({ $primary }) => ($primary ? "white" : "#407BFF")};
-  background: ${({ $primary }) => ($primary ? "#407BFF" : "white")};
-  border: 2px solid #407BFF;
-`;
-
-const LocationPermissionPopup: React.FC<LocationPermissionPopupProps> = ({
-  open,
-  onClose,
-  onAllow,
-  onDeny,
-}) => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return null; // Mencegah render saat SSR
+const LocationPermissionPopup: React.FC<LocationPermissionPopupProps> = ({ open, onClose, onAllow, onDeny }) => {
+  if (typeof window === "undefined") return null; // Cek apakah di server-side
 
   return open ? (
     <Overlay>
@@ -88,9 +25,7 @@ const LocationPermissionPopup: React.FC<LocationPermissionPopupProps> = ({
         <Message>Fitur ini memerlukan akses lokasi Anda. Izinkan akses?</Message>
         <ButtonContainer>
           <Button onClick={onDeny}>Tolak</Button>
-          <Button $primary={true} onClick={onAllow}>
-            Izinkan
-          </Button>
+          <Button $primary onClick={onAllow}>Izinkan</Button>
         </ButtonContainer>
       </Modal>
     </Overlay>

2b10c75af0c0cd00a08787b95fcc1712d537f5f1
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2025-03-02 23:28:41 +0700

    [CHORES] Separate Jest Babel config to avoid conflict with Next.js SWC

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,23 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { configFile: "./jest/babel.config.js" }],
   },
-  transformIgnorePatterns: [
-    "node_modules/(?!@amcharts)"
-  ],
+  transformIgnorePatterns: ["node_modules/(?!@amcharts)"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^@amcharts/amcharts5$": "<rootDir>/__mocks__/amcharts5.js",
     "^@amcharts/amcharts5/map$": "<rootDir>/__mocks__/amcharts5-map.js",
     "^@amcharts/amcharts5-geodata/indonesiaLow$": "<rootDir>/__mocks__/amcharts5-geodata.js",
+    "^@amcharts/amcharts5/themes/Animated$": "<rootDir>/__mocks__/amcharts5-themes-Animated.js",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
+
+module.exports = createJestConfig(customJestConfig);

da113273b35cf420ca5dd6bb2d9a0568b7501e34
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2025-03-03 17:40:17 +0700

    [RED] added map component and page to render map

---
+++ app/map/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import IndonesiaMap from "../components/IndonesiaMap";
+
+export default function MapPage() {
+  return (
+    <div>
+      <IndonesiaMap />
+    </div>
+  );
+}

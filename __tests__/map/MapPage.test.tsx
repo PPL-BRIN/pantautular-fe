f9a92eaaebe741f43267a5da7bc6ed714a607b68
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2025-03-03 13:40:03 +0700

    [REFACTOR] Removed unused imports in MapPage.test

--- __tests__/map/MapPage.test.tsx
+++ __tests__/map/MapPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import MapPage from "../../app/map/page";
 

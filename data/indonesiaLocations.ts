9433dc6cd12c6f513e9a2f70e0c6159ee0bb62dc
alvaro.luqman1 <alvaro.luqman@gmail.com>
2025-03-09 19:24:32 +0700

    [REFACTOR] Update default zoom level and adjust minimum zoom level in map chart service

--- data/indonesiaLocations.ts
+++ data/indonesiaLocations.ts
@@ -1,6 +1,6 @@
 import { MapConfig } from "../types";
 
   export const defaultMapConfig: MapConfig = {
-    zoomLevel: 2.6,
+    zoomLevel: 10.6,
     centerPoint: { longitude: 113.9213, latitude: 0.0 }
   };

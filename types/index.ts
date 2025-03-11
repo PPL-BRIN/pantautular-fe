42e3378b0167d0d829feacd6e6eb51d2ed5ea04b
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-03-12 00:33:07 +0700

    [REFACTOR]: ensure consistency

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,12 @@ export interface MapConfig {
   zoomLevel: number;
   centerPoint: { longitude: number; latitude: number };
 }
+
+export interface FilterState {
+  diseases: string[];
+  locations: string[];
+  level_of_alertness: number;
+  portals: string[];
+  start_date: null | Date;
+  end_date: null | Date;
+}

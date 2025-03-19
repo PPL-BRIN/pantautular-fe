8b0b0761ee55e0a49e46d570beabb270ad8cd8f1
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-19 23:45:45 +0700

    Implement Dashboard Page -Green

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,2 +1,24 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import FilterSection from "../components/dashboard/FilterSection";
+import InformationSection from "../components/dashboard/InformationSection";
+
+const Page = () => {
+    return (
+        <div className="h-screen bg-[#ebf3f5]">
+            <Navbar />
+            <div className="h-full flex w-full gap-5">
+                <div className="w-2/5 bg-transparent">
+                    <FilterSection />
+                </div>
+                <div className="w-3/5 bg-transparent">
+                    <InformationSection />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
 
 

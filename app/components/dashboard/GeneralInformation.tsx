a54828155d515f25b435b8dac57c6132ad12fe8f
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-19 23:01:03 +0700

    General Information Section - GREEN

---
+++ app/components/dashboard/GeneralInformation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const GeneralInformation = () => {
+    return (
+        <div className="flex items-center justify-center bg-transparent text-black text-lg p-6 rounded-lg shadow-md border">
+            <p>Informasi Kasus Penyakit Menular</p>
+        </div>
+    );
+};
+
+export default GeneralInformation
+

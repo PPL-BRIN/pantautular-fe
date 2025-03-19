cc906236cabb0804d119cad6cf2692006369a6f7
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-19 22:51:32 +0700

    Filter Section Component - RED

---
+++ __tests__/dashboard/FilterSection.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import FilterSection from "../../app/components/dashboard/FilterSection"// Adjust the path based on your file structure
+
+describe("FilterSection Component", () => {
+  it("renders correctly with the expected text", () => {
+    render(<FilterSection />);
+
+    // Check if the text "Place Filter Here" is present in the document
+    const textElement = screen.getByText(/Place Filter Here/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("applies the correct CSS classes", () => {
+    render(<FilterSection />);
+    
+    const divElement = screen.getByText(/Place Filter Here/i);
+    expect(divElement).toHaveClass("flex", "items-center", "justify-center", "bg-transparent", "text-xl", "p-4", "rounded-lg", "pt-10", "text-black");
+  });
+});

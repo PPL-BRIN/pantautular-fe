fd27e598b9378e81d1540bd6bcc0d7d113d400c4
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-19 23:06:21 +0700

    Implement Cases Order Section (Added Test Cases) - RED

--- __tests__/dashboard/CasesOrder.test.tsx
+++ __tests__/dashboard/CasesOrder.test.tsx
@@ -1,3 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CasesOrder from "../../app/components/dashboard/CasesOrder"
+
+describe("CasesOrder Component", () => {
+    it("renders correctly with the expected text", () => {
+      render(<CasesOrder />);
+  
+      const textElement = screen.getByText(/Urutan Kasus/i);
+      expect(textElement).toBeInTheDocument();
+    });
+  
+    it("applies the correct CSS classes", () => {
+      render(<CasesOrder />);
+      
+      const divElement = screen.getByText(/Urutan Kasus/i).parentElement;
+      expect(divElement).toHaveClass("flex", "items-center", "justify-center", "bg-transparent", "text-black", "text-lg", "p-6", "rounded-lg", "shadow-md", "border");
+    });
+  });

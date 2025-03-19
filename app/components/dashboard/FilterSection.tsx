5301f2d9e89e6a22f0abe790369d271f215c6ba6
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-19 22:54:47 +0700

    Filter Section Component - GREEN

---
+++ app/components/dashboard/FilterSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FilterSection = () => {
+    return (
+        <div className="flex items-center justify-center bg-transparent text-xl p-4 rounded-lg pt-10 text-black">
+            Place Filter Here
+        </div>
+    );
+};
+
+export default FilterSection;

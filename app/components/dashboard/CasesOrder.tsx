ee6b3276d4934303a342957ed7ebd23e4ff004c7
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-03-19 23:07:21 +0700

    Implement Cases Order Section (Added Test Cases) - GREEN

--- app/components/dashboard/CasesOrder.tsx
+++ app/components/dashboard/CasesOrder.tsx
@@ -1,2 +1,11 @@
 import React from "react";
 
+const CasesOrder = () => {
+    return (
+        <div className="flex items-center justify-center bg-transparent text-black text-lg p-6 rounded-lg shadow-md border">
+            <p>Urutan Kasus</p>
+        </div>
+    );
+};
+
+export default CasesOrder

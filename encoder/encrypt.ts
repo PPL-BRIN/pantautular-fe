21de00559de46f9124b1ee65bbfefd0a7dda46bf
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-05-12 23:39:06 +0700

    Refactor test -GREEN

--- encoder/encrypt.ts
+++ encoder/encrypt.ts
@@ -2,12 +2,11 @@ import { randomBytes, createCipheriv } from "crypto";
 
 export function encrypt(text: string, key: string): string {
   const keyBuffer = Buffer.from(key, 'utf8');
-  const iv = randomBytes(12);
-  const cipher = createCipheriv('aes-256-gcm', keyBuffer, iv);
+  const rand = randomBytes(12);
 
+  const cipher = createCipheriv('aes-256-gcm', keyBuffer, rand);
   const encrypted = Buffer.concat([cipher.update(text, 'utf8'), cipher.final()]);
   const tag = cipher.getAuthTag();
-
-  const payload = Buffer.concat([iv, tag, encrypted]).toString('base64');
+  const payload = Buffer.concat([rand, tag, encrypted]).toString('base64');
   return payload;
 }

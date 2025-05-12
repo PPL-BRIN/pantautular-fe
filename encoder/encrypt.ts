e39a2fd4893ab2801d0508fe5df3bf9a6930e45a
KenBalya <124903836+KenBalya@users.noreply.github.com>
2025-05-12 23:35:40 +0700

    Implement encoders -GREEN

---
+++ encoder/encrypt.ts
@@ -0,0 +1,13 @@
+import { randomBytes, createCipheriv } from "crypto";
+
+export function encrypt(text: string, key: string): string {
+  const keyBuffer = Buffer.from(key, 'utf8');
+  const iv = randomBytes(12);
+  const cipher = createCipheriv('aes-256-gcm', keyBuffer, iv);
+
+  const encrypted = Buffer.concat([cipher.update(text, 'utf8'), cipher.final()]);
+  const tag = cipher.getAuthTag();
+
+  const payload = Buffer.concat([iv, tag, encrypted]).toString('base64');
+  return payload;
+}
